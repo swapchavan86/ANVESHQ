0714bf29fd898549a4f9f2bb1c7d5cdb944f89e6
swapnilchavan <swapnilc1@hexaware.com>
2026-02-10 11:29:14 +0530

    update master

--- anveshq/src/app/history/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HistoryPage() {
-  return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-6">History</h1>
-      <p>This page will show the history of your paper trades.</p>
-    </div>
-  );
-}
